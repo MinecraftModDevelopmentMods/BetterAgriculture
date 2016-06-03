d233ca5d744a787bd2b80cb2ef76b9566fc454cd
knoxhack <hackofgod@gmail.com>
2016-06-03 00:01:13 -0400

    left over

--- src/main/java/betteragriculture/client/render/mobs/RenderEntityMobPig6.java
+++ src/main/java/betteragriculture/client/render/mobs/RenderEntityMobPig6.java
@@ -1,29 +1,26 @@
 package betteragriculture.client.render.mobs;
 
-import betteragriculture.client.model.ModelEntityBoss1;
-import betteragriculture.client.model.ModelUndeadGhastly;
-import betteragriculture.entity.entityboss.EntityBoss1;
-import betteragriculture.entity.entitymob.UndeadGhastly;
-
+import betteragriculture.entity.entitymob.EntityMobPig6;
+import net.minecraft.client.model.ModelPig;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderEntityMobPig6 extends RenderLiving<EntityBoss1>
+public class RenderEntityMobPig6 extends RenderLiving<EntityMobPig6>
 {
-	private final ResourceLocation textures = new ResourceLocation("netherexpansion:textures/models/UndeadGhastly.png");
+	private final ResourceLocation textures = new ResourceLocation("betteragriculture:textures/models/pig2.png");
 
 	public RenderEntityMobPig6(RenderManager renderManager)
 	{
-		super(renderManager, new ModelEntityBoss1(), 0);
+		super(renderManager, new ModelPig(), 0);
 		this.addLayer(new LayerHeldItem(this));
 	}
 
 	
 
 	@Override
-	protected ResourceLocation getEntityTexture(EntityBoss1 entity)
+	protected ResourceLocation getEntityTexture(EntityMobPig6 entity)
 	{
 		return textures;
 	}
