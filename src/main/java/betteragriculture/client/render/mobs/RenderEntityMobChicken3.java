b75f6ec04abf01a67932fab2b7262d80affeeb42
knoxhack <hackofgod@gmail.com>
2016-06-02 21:12:50 -0400

    Added classes for all mob entities
    Model, Render, Entity classes

---
+++ src/main/java/betteragriculture/client/render/mobs/RenderEntityMobChicken3.java
@@ -0,0 +1,30 @@
+package betteragriculture.client.render.mobs;
+
+import betteragriculture.client.model.ModelEntityBoss1;
+import betteragriculture.client.model.ModelUndeadGhastly;
+import betteragriculture.entity.entityboss.EntityBoss1;
+import betteragriculture.entity.entitymob.UndeadGhastly;
+
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderEntityMobChicken3 extends RenderLiving<EntityBoss1>
+{
+	private final ResourceLocation textures = new ResourceLocation("netherexpansion:textures/models/UndeadGhastly.png");
+
+	public RenderEntityMobChicken3(RenderManager renderManager)
+	{
+		super(renderManager, new ModelEntityBoss1(), 0);
+		this.addLayer(new LayerHeldItem(this));
+	}
+
+	
+
+	@Override
+	protected ResourceLocation getEntityTexture(EntityBoss1 entity)
+	{
+		return textures;
+	}
+}
---
+++ src/main/java/betteragriculture/entity/entitymob/EntityMobPig1.java
@@ -0,0 +1,48 @@
+package betteragriculture.entity.entitymob;
+
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.*;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+
+
+public class EntityMobPig1 extends EntityMob {
+
+	public EntityMobPig1(World worldIn)
+	{
+		super(worldIn);
+
+		this.setSize(0.9F, 1.5F);
+		this.experienceValue = 5;
+		this.tasks.addTask(2, new EntityAIWander(this, this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue()));
+		this.tasks.addTask(3, new EntityAISwimming(this));
+		this.tasks.addTask(4, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+		this.tasks.addTask(5, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
+	}
+
+
+
+	protected boolean isAIEnabled()
+	{
+		return true;
+	}
+
+	protected void applyEntityAttributes()
+	{
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(30.0F);
+		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.5D);
+	}
+
+
+
+
+
+
+			
+		
+	
+}
