7b0bafda68f6c42e869c1114d53dda400611c9ca
knoxhack <hackofgod@gmail.com>
2016-06-04 07:58:11 -0400

    version 0.3

--- src/main/java/betteragriculture/Main.java
+++ src/main/java/betteragriculture/Main.java
@@ -75,7 +75,7 @@ public class Main {
 
 	public static final String MODID = "betteragriculture";
 	public static final String MODNAME = "Better Agriculture";
-	public static final String VERSION = "0.2";
+	public static final String VERSION = "0.3";
 	private static int entityID = 0;
 
 
--- src/main/java/betteragriculture/entity/entitymob/EntityMobCow5.java
+++ src/main/java/betteragriculture/entity/entitymob/EntityMobCow5.java
@@ -1,48 +1,116 @@
 package betteragriculture.entity.entitymob;
 
+import javax.annotation.Nullable;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
-import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.storage.loot.LootTableList;
 
 
 
-public class EntityMobCow5 extends EntityMob {
+public class EntityMobCow5 extends EntityAnimal
+{
+    public EntityMobCow5(World worldIn)
+    {
+        super(worldIn);
+        this.setSize(0.9F, 1.4F);
+    }
 
-	public EntityMobCow5(World worldIn)
-	{
-		super(worldIn);
+    protected void initEntityAI()
+    {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIPanic(this, 2.0D));
+        this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.25D, Items.WHEAT, false));
+        this.tasks.addTask(4, new EntityAIFollowParent(this, 1.25D));
+        this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(7, new EntityAILookIdle(this));
+    }
 
-		this.setSize(0.9F, 1.5F);
-		this.experienceValue = 5;
-		this.tasks.addTask(2, new EntityAIWander(this, this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getAttributeValue()));
-		this.tasks.addTask(3, new EntityAISwimming(this));
-		this.tasks.addTask(4, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
-		this.tasks.addTask(5, new EntityAILookIdle(this));
-		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
-	}
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.20000000298023224D);
+    }
 
+    protected SoundEvent getAmbientSound()
+    {
+        return SoundEvents.ENTITY_COW_AMBIENT;
+    }
 
+    protected SoundEvent getHurtSound()
+    {
+        return SoundEvents.ENTITY_COW_HURT;
+    }
 
-	protected boolean isAIEnabled()
-	{
-		return true;
-	}
+    protected SoundEvent getDeathSound()
+    {
+        return SoundEvents.ENTITY_COW_DEATH;
+    }
 
-	protected void applyEntityAttributes()
-	{
-		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(30.0F);
-		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.5D);
-	}
+    protected void playStepSound(BlockPos pos, Block blockIn)
+    {
+        this.playSound(SoundEvents.ENTITY_COW_STEP, 0.15F, 1.0F);
+    }
 
+    /**
+     * Returns the volume for the sounds this mob makes.
+     */
+    protected float getSoundVolume()
+    {
+        return 0.4F;
+    }
 
+    @Nullable
+    protected ResourceLocation getLootTable()
+    {
+        return LootTableList.ENTITIES_COW;
+    }
 
+    public boolean processInteract(EntityPlayer player, EnumHand hand, @Nullable ItemStack stack)
+    {
+        if (stack != null && stack.getItem() == Items.BUCKET && !player.capabilities.isCreativeMode && !this.isChild())
+        {
+            player.playSound(SoundEvents.ENTITY_COW_MILK, 1.0F, 1.0F);
 
+            if (--stack.stackSize == 0)
+            {
+                player.setHeldItem(hand, new ItemStack(Items.MILK_BUCKET));
+            }
+            else if (!player.inventory.addItemStackToInventory(new ItemStack(Items.MILK_BUCKET)))
+            {
+                player.dropItem(new ItemStack(Items.MILK_BUCKET), false);
+            }
 
+            return true;
+        }
+        else
+        {
+            return super.processInteract(player, hand, stack);
+        }
+    }
 
-			
-		
-	
+    public EntityMobCow5 createChild(EntityAgeable ageable)
+    {
+        return new EntityMobCow5(this.worldObj);
+    }
+
+    public float getEyeHeight()
+    {
+        return this.isChild() ? this.height : 1.3F;
+    }
 }
