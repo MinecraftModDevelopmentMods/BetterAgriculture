0e560024ebea2fe38442899d9cf80c861c892c68
knoxhack <hackofgod@gmail.com>
2016-06-04 09:27:38 -0400

    version 0.4

--- src/main/java/betteragriculture/Main.java
+++ src/main/java/betteragriculture/Main.java
@@ -75,7 +75,7 @@ public class Main {
 
 	public static final String MODID = "betteragriculture";
 	public static final String MODNAME = "Better Agriculture";
-	public static final String VERSION = "0.3";
+	public static final String VERSION = "0.4";
 	private static int entityID = 0;
 
 
