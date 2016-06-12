52e1b971c3bcc2390350334e37efc206a0d0a76c
knoxhack <hackofgod@gmail.com>
2016-06-12 13:48:11 -0400

    first

--- src/main/java/betteragriculture/Main.java
+++ src/main/java/betteragriculture/Main.java
@@ -81,7 +81,7 @@ public class Main {
 	public static final String VERSION = "0.13";
 	private static int entityID = 0;
 
-//	private FarmlandBiome farmlandBiome;
+	private FarmlandBiome farmlandBiome2;
 
 
 	
