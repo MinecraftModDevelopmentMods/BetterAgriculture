3250dad6b2c061266889cb63cbf52e9c626dc23e
knoxhack <hackofgod@gmail.com>
2016-06-23 23:15:15 -0400

    v0.15

--- src/main/java/betteragriculture/Main.java
+++ src/main/java/betteragriculture/Main.java
@@ -78,7 +78,7 @@ public class Main {
 
 	public static final String MODID = "betteragriculture";
 	public static final String MODNAME = "Better Agriculture";
-	public static final String VERSION = "0.14";
+	public static final String VERSION = "0.15";
 	private static int entityID = 0;
 
 //	private FarmlandBiome farmlandBiome;
