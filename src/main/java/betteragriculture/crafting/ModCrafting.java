f593564977cf21cec24fce93f6d0779d5d3f90fa
knoxhack <hackofgod@gmail.com>
2016-08-23 16:04:36 -0400

    For for issue #20

--- src/main/java/betteragriculture/crafting/ModCrafting.java
+++ src/main/java/betteragriculture/crafting/ModCrafting.java
@@ -12,23 +12,10 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public final class ModCrafting {
 
 	public static void initCrafting() {
-		// Vanilla
-		GameRegistry.addSmelting(ModBlocks.nethercoalOre, new ItemStack(Blocks.COAL_ORE, 2), 1.0f);
 
-		
-		
-		
 		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.concreteBlock, 6), new Object[] {Blocks.SAND, Blocks.STONE,Items.WATER_BUCKET});
 		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.rooftileBlock, 2), new Object[] {ModItems.slateItem, ModItems.slateItem,ModItems.slateItem,ModItems.slateItem});
-
-		
-		
-		
 		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.wireItem, 6), new Object[] {Items.GOLD_NUGGET});
-
-		
-		
-		
 		
 		GameRegistry.addRecipe(new ItemStack(ModBlocks.redsidingBlock, 8), new Object[] {"###", "#B#", "###", '#', Blocks.PLANKS, 'B', new ItemStack(Items.DYE, 1, 1)});
 		GameRegistry.addRecipe(new ItemStack(ModBlocks.whitesidingBlock, 8), new Object[] {"###", "#B#", "###", '#', Blocks.PLANKS, 'B', new ItemStack(Items.DYE, 1, 15)});
@@ -41,25 +28,6 @@ public final class ModCrafting {
 		GameRegistry.addRecipe(new ItemStack(ModBlocks.sprucetroughBlock, 4), new Object[] {"# #", "#B#", "###", '#',  new ItemStack(Blocks.PLANKS, 1, 1), 'B',Items.WATER_BUCKET});
 		GameRegistry.addRecipe(new ItemStack(ModBlocks.stonetroughBlock, 4), new Object[] {"# #", "#B#", "###", '#',  Blocks.STONE, 'B',Items.WATER_BUCKET});
 
-		GameRegistry.addRecipe(new ItemStack(ModBlocks.barnroofBlock, 8), new Object[] {"###", "#B#", "###", '#', Blocks.PLANKS, 'B', new ItemStack(Items.DYE, 1, 2)});
-
-
-		
-		
-		
-		
-	
-		
-		
-		
-		
-
-		// Base Metals
-		if(Loader.isModLoaded("basemetals")) {
-//			GameRegistry.addSmelting(ModBlocks.netherantimonyOre, new ItemStack(BmBlocks.antimony_ore, 2), 1.0f);	// Not supported in BM 2.2.2
-
-		}
-
-		
+		GameRegistry.addRecipe(new ItemStack(ModBlocks.barnroofBlock, 8), new Object[] {"###", "#B#", "###", '#', Blocks.PLANKS, 'B', new ItemStack(Items.DYE, 1, 2)});	
 	}
 }
