bd394ea0d1309d46d7809bcce856c634cbd1758b
knoxhack <hackofgod@gmail.com>
2016-11-27 18:46:19 -0500

    fixed cows droping slate

--- src/main/java/betteragriculture/Main.java
+++ src/main/java/betteragriculture/Main.java
@@ -83,7 +83,7 @@ public class Main {
     public static CreativeTabs tab;
 	public static final String MODID = "betteragriculture";
 	public static final String MODNAME = "Better Agriculture";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	private static int entityID = 0;
 
 
